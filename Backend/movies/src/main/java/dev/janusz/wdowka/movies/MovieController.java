7632d46de7f58576a3e45c96b61b27409dc82e28
Janusz Wdowka <j.wdowka2000@gmail.com>
2023-10-24 20:41:51 +0200

    implementacja połączenia reacta z javą

--- Backend/movies/src/main/java/dev/janusz/wdowka/movies/MovieController.java
+++ Backend/movies/src/main/java/dev/janusz/wdowka/movies/MovieController.java
@@ -4,10 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +13,7 @@ import java.util.Optional;
  * Api Controller obiektu Movie
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/movies")
 public class MovieController {
     @Autowired
--- Frontend/movies-review/src/App.js
+++ Frontend/movies-review/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [movies, setMovies] = useState();
+  const getMovies = async () => {
+    try {
+      const response = await api.get('/api/movies');
+      console.log(response.data);//pokazanie danych
+      setMovies(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
